e16631a626336e3609a00d01e05b9df9262ec7ad
Oleg-max-tech <olehkostiv2003@gmail.com>
2025-02-13 13:03:37 +0200

    !

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,23 @@
-// import type { Metadata } from "next";
-// import "./globals.css";
+import type { Metadata } from "next";
+import "./globals.css";
+import Header from "./Components/Header";
 
-// export const metadata: Metadata = {
-//   title: "My Next.js App",
-//   description: "Example Next.js app",
-// };
+export const metadata: Metadata = {
+  title: "My Next.js App",
+  description: "Example Next.js app",
+};
 
-// export default function RootLayout({ children }: { children: React.ReactNode }) {
-//   return (
-//     <html lang="uk">
-//       <body className="flex flex-col min-h-screen">
-//         <Header />
-//         {children}
-//       </body>
-//     </html>
-//   );
-// }
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="uk">
+      <body className="flex flex-col min-h-screen">
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  redirect("/home"); // Перенаправлення на головну сторінку
-  return null; // Повертаємо null, оскільки редирект вже стався
+  redirect("/home");
+  return null;
 }

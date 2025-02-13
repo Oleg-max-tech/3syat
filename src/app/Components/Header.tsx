d9cbbdf011c17a52d18569a74e72c7d0b321de5c
Oleg-max-tech <olehkostiv2003@gmail.com>
2025-02-13 14:40:38 +0200

    !

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -1,16 +1,11 @@
 "use client";
 
 import React from "react";
-import Link from "next/link"; // Використовуємо Link з Next.js
-
+import Link from "next/link";
 const Header = () => {
   return (
     <div className="flex justify-between items-center bg-yellow-400 text-white p-4 shadow-lg">
-      <img
-        src="../Components/img/logo.png"
-        alt="logo company"
-        className="w-24 h-auto"
-      />
+      <img src="../img/logo.png" alt="logo company" className="w-24 h-auto" />
       <div className="flex gap-6 text-lg font-bold">
         <Link href="/page1" className="hover:text-yellow-600">
           Послуги
